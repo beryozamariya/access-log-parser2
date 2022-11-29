aeb3a5dafd4c18409a46cbd59e1b939d50105d2b
beryozamariya <beryoza.mariya@gmail.com>
2022-11-29 19:05:50 +0300

    'Сalculation'

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число");
+        int Number = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int Number2 = new Scanner(System.in).nextInt();
+        {
+            String str = "qwerty";
+            int x = Number;
+            int y = Number2;
+            int sum = x + y;
+            int sum1 = x * y;
+            int sum2 = x - y;
+            double sum3 = (double) x / y;
+            System.out.println("Сумма " + sum);
+            System.out.println("Произведение " + sum1);
+            System.out.println("Разность " + sum2);
+            System.out.println("Частное " + sum3);
+        }
     }
 }
+
+
